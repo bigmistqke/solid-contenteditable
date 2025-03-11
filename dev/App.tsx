6f9f33a6f56792eccd3738106ca463b9748858b0
bigmistqke <vandijckv@gmail.com>
2025-03-11 02:34:16 +0100

    update demo

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,34 +1,59 @@
-import { For, Show } from 'solid-js'
+import { For, Show, type Accessor, type JSX } from 'solid-js'
 import { ContentEditable } from '../src'
+
+function Split(props: {
+  value: string
+  delimiter: string
+  children: (value: string, index: Accessor<number>) => JSX.Element
+}) {
+  return (
+    <For each={props.value.split(props.delimiter)}>
+      {(value, index) => {
+        const isLast = () => index() === props.value.split(props.delimiter).length - 1
+        return (
+          <>
+            {props.children(value, index)}
+            <Show when={!isLast()}>{props.delimiter}</Show>
+          </>
+        )
+      }}
+    </For>
+  )
+}
+
+function Mentions(props: { multiline?: boolean }) {
+  return (
+    <ContentEditable
+      value="     #hallo    #test"
+      style={{ padding: '10px' }}
+      multiline={props.multiline}
+    >
+      {value => (
+        <Split value={value} delimiter={'\n'}>
+          {line => (
+            <Split value={line} delimiter={' '}>
+              {word => (
+                <Show when={word.startsWith('#')} fallback={word}>
+                  <button onClick={() => console.log('ok')} style={{ margin: '0px' }}>
+                    {word}
+                  </button>
+                </Show>
+              )}
+            </Split>
+          )}
+        </Split>
+      )}
+    </ContentEditable>
+  )
+}
 
 export function App() {
   return (
-    <ContentEditable value="     #hallo    #test" style={{ padding: '10px' }}>
-      {value => (
-        <For each={value.split('\n')}>
-          {(line, lineIndex) => {
-            const isLastLine = () => value.split('\n').length - 1 === lineIndex()
-            return (
-              <>
-                <For each={line.split(' ')}>
-                  {(word, wordIndex) => {
-                    const isLastWord = () => line.split(' ').length - 1 === wordIndex()
-                    return (
-                      <>
-                        <Show when={word.startsWith('#')} fallback={word}>
-                          <button onClick={() => console.log('clicked!')}>{word}</button>
-                        </Show>
-                        <Show when={!isLastWord()}> </Show>
-                      </>
-                    )
-                  }}
-                </For>
-                <Show when={!isLastLine()}>{'\n'}</Show>
-              </>
-            )
-          }}
-        </For>
-      )}
-    </ContentEditable>
+    <>
+      <ContentEditable multiline={false} value="     #hallo    #test" style={{ padding: '10px' }} />
+      <ContentEditable value="     #hallo    #test" style={{ padding: '10px' }} />
+      <Mentions multiline={false} />
+      <Mentions />
+    </>
   )
 }
